26bcd6f7bf6e4ccbb700dd21447d4c23d644202a
charles-mrt <charlesmartins.ads@gmail.com>
2024-06-17 17:27:40 -0300

    feat: server config

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { projectRoutes } from './routes/project.routes'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
+
+app.register(projectRoutes, {
+  prefix: '/project',
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server runing on http://localhost:3333')
+  })
