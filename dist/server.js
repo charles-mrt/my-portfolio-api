eb02a48a0ad8af394775ea56320bfb28011b0357
charles-mrt <charlesmartins.ads@gmail.com>
2024-07-12 15:19:01 -0300

    fix: error build

--- dist/server.js
+++ dist/server.js
@@ -6,17 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
 const cors_1 = __importDefault(require("@fastify/cors"));
 const project_routes_1 = require("./routes/project.routes");
-const app = (0, fastify_1.default)();
+const app = (0, fastify_1.default)({
+    logger: true
+});
 app.register(cors_1.default, {
     origin: true,
 });
 app.register(project_routes_1.projectRoutes, {
     prefix: '/projects',
 });
-app
-    .listen({
-    port: 3333,
-})
-    .then(() => {
-    console.log('🚀 HTTP server runing on http://localhost:3333');
-});
+const start = async () => {
+    try {
+        await app.listen();
+        console.log('🚀 HTTP server running');
+    }
+    catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+start();
